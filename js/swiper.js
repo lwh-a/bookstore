065b81d288419744f205605989ec2b6a1f5b4439
lwh-a <nananatehan1133@naver.com>
2026-03-19 15:32:24 +0900

    20260319_slider

---
+++ js/swiper.js
@@ -0,0 +1,25 @@
+var slider_swiper = new Swiper(".sliderSwiper", {
+    navigation: {
+        nextEl: "#slider .swiper-button-next",
+        prevEl: "#slider .swiper-button-prev",
+    },
+    pagination: {
+        el: "#slider .swiper-pagination",
+        clickable: true,
+    },
+});
+
+// #new swiper
+var new_swiper = new Swiper(".newSwiper", {
+      slidesPerView: 5,
+      spaceBetween: 10,
+      pagination: {
+        el: "#new .swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: "#new .swiper-button-next",
+        prevEL: "#new .swiper-button-prev", 
+      },
+      slidesPerGroup: 5
+    });
